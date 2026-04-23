e02c46a6cfa5b3f45f3849100ec3d0affafbf46e
tejashtandel <tejas@itidoltechnologies.com>
2026-04-23 17:58:38 +0530

    Fix Fastify DB plugin

--- config/db.js
+++ config/db.js
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
+import fp from 'fastify-plugin';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-const connectDB = async () => {
+async function connectDB(fastify, options) {
   try {
     console.log('🔗 Connecting to MongoDB...');
     console.log(`🔗 Connection string: ${process.env.MONGODB_STRING}`);
 
     await mongoose.connect(process.env.MONGODB_STRING);
+
     console.log('✅ MongoDB connected with Mongoose');
   } catch (err) {
-    console.error(err);
+    console.error('❌ MongoDB connection error:', err);
     process.exit(1);
   }
-};
+}
 
-export default connectDB;
+export default fp(connectDB);
