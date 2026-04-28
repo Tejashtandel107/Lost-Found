bac07707653f9640145f76c11ed74921fab3480a
tejashtandel <tejas@itidoltechnologies.com>
2026-04-28 10:22:00 +0530

    remove unuse code

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,10 @@
 import authRoutes from '../modules/auth/controller.js';
 import itemRoutes from '../modules/item/controller.js';
-import reportRoutes from '../modules/report/controller.js';
 import uploadRoutes from '../modules/upload/controller.js';
 
 async function routes(app, options) {
   app.register(authRoutes, { prefix: '/auth' });
   app.register(itemRoutes, { prefix: '/items' });
-  app.register(reportRoutes, { prefix: '/reports' });
   app.register(uploadRoutes);
 }
 
